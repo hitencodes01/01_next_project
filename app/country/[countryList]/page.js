4b3c714ad60e09c72cbca8b43a7a04b503d7f177
hitencodes01 <hitenunnao@gmail.com>
2025-06-19 12:41:40 +0530

    build project

--- app/country/[countryList]/page.js
+++ app/country/[countryList]/page.js
@@ -1,6 +1,5 @@
 import Card from "@/components/Card"
 import newsData from "@/data/newsData"
-import styles from "../../../components/CardContainer.module.css"
 import Header from "@/components/Header"
 import Footer from "@/components/Footer"
 export default async function CountryList({params}) {
@@ -10,7 +9,7 @@ export default async function CountryList({params}) {
   return (
     <div>
         <h1>Country : {countryList.toUpperCase()}</h1>
-        <ul className={styles["card-container"]} >
+        <ul className="card-container" >
             {news.map(item => <li key={item.id}>
               <Card title={item.title} body={item.body} category={item.category} />
             </li>)}
